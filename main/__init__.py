6e8aaa5d4a4cda3ec4d678d1d0ca2808dcab7077
LinnJonss <155692722+LinnJonss@users.noreply.github.com>
2024-03-26 09:18:06 +0100

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,6 @@
 '''
 Logga in ska vi ej ha 
 
- 
 I första versionen kanske den ska innehålla följande, varav alla kommer vara med i final version på något sätt: 
 - schemavy: dag 
 - uppgifter: kommande uppgifter för två olika kurser, innehållande namn på uppg, startdatum och deadline och 
@@ -22,53 +21,71 @@ skapa en lista för varje ny användare med deras nya namn och inkludera det som
 '''namn GLOBAL VARIABEL? SÅLÄNGE'''
 
 def main():
-    namn = input("Hej vad heter du? ")
+    namn = input("Hej! vad heter du? ")
+    användaruppgifter = [namn]
     print(f"Välkommen {namn} till BeWisePlanners verktyg för studieplanering!")
-
+    menu()
+ 
+def menu():
     print("Vad vill du göra? Ange siffran för menyvalet du vill besöka")
 
-    print("1) Se veckoschema schema över kommande uppgifter")
-    print("2) Se alla uppgifter")
-    print("3) Se statistik över dina studier")
-    print("4) Hantera befintliga kurser")
-    print("5) Lägg till nya kurser")
-    print("6) Hantera befintliga uppgifter")
-    print("7) Lägg till nya uppgifter")
-   
-    
+    print("3) Lägg till nya kurser")
+    print("2) Se dina kurser")
+    print("2) Lägg till nya uppgifter inom valfri kurs")
+    print("3) Se dina uppgifter")
+
     menyval = input("Ditt val: ")
    
     while True:
-        
         if menyval == "1":
-            pass 
+            add_courses()
         elif menyval == "2":
-            pass 
+            view_courses()
         elif menyval == "3":
-            pass 
+            add_assignments()
         elif menyval == "4":
-            pass 
-        elif menyval == "5":
-            pass
-        else: 
+            view_assignments()
+        else:
             print("Menyvalet existerar inte, se lista och testa igen!")
             break 
 
-def view_weekly_planning():
-    
+def add_courses():
+    print("LÄGG TILL KURSER")
+ 
+    ny_kurs = input("Vad heter kursen du vill lägga till? ")
+    #lägg till kursen i namn listan 
+    aktiv_period = input("Hur länge pågår kursen? Ange antal veckor: ")
+    #lägg till perioden i namn listan
 
-def view_all_assignments():
-    print("ALLA DINA UPPGIFTER")
-    print("*"x40)
+    print("Vill du lägga till fler kurser")
+    print("Ange 1 för att lägga till fler eller skriv valfri symbol för att komma tillbaka till huvudmenyn")
+    kurs_fråga = input("Ditt val: ")
 
-    print("Vad vill du se? Ange")
-    print("1) Visa alla avklarade och ej avklarade uppgifter oberoende av kurs")
-    print("2) Filtrera efter kurs")
-    print("3) Filtrera efter avklarade")
-    print("4) Filtrera efter ej avklarade")
+    while True:
 
+     if kurs_fråga == "1":
+       ny_kurs = input("Vad heter kursen du vill lägga till? ")
+       #lägg till kursen i namn listan - TRY DVS CSV ÄR NOG LÄTTAST ? MINNS EJ HUR HÄMTA INOM DOKUMENTET OM EJ 
+       aktiv_period = input("Hur länge pågår kursen? Ange antal veckor: ")
+       #lägg till perioden i namn listan
 
-def manage_existing_courses():
+     else: 
+        menu() 
+
+def view_courses():
+   print("SE KURSER")
+   #print alla kurser personen har 
+
+   #vill du lägga till uppgifter till en viss kurs? 1) ja gå till uppgifter 2) nej meny 
+ 
+
+def add_assignments():
+
+    print("LÄGG TILL UPPGIFTER")
+    #börja med att söka i listan efter kurser under personen och finns det ej hänvisa tillbaka till menyn eller låt dem välja lägga till kurser
+
+    print("
+
 
     print("Här kan du hantera dina befintliga kurser och lägga till nya")
     changed_course = input("Vilken kurs vill du redigera?" )
