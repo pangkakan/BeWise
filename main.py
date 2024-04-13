e76201412b1faeebd3ea52429fe7a30f9b8b3d3f
Marcus Dentz <marcus.dentz93@gmail.com>
2024-04-13 21:44:54 +0200

    Lagt till delar av grundläggande funktionalitet till RS2

--- main.py
+++ main.py
@@ -2,16 +2,88 @@ from bottle import route, get, post, run, template, error, static_file, request,
 import json
 
 
+# Läser från json-fil och returnerar lista av innehåll
+def read_from_json_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+        
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found. Creating a new file.")
+        with open(file_path, 'w') as file:
+            json.dump([], file)
+            return []
+        
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON in file '{file_path}'.")
+        return []
+   
+
+# Skriver lista av innehåll till json-fil
+def save_to_json_file(data, file_path):
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Data saved to '{file_path}' successfully.")
+
+    except Exception as e:
+        print(f"Error saving data to '{file_path}': {e}")
+
+
+
+
+def get_course_with_coursecode(coursecode):
+    courses = read_from_json_file("static/courses.json")
+    for course in courses:
+        if course["kurskod"] == coursecode:
+            return course
+        
+
+def get_tasks_with_coursecode(coursecode):
+    tasks = read_from_json_file("static/tasks.json")
+    task_list = []
+    for task in tasks:
+        if task["kurskod"] == coursecode:
+            task_list.append(task)
+    
+    return task_list
+
+
+def get_timeblocks_with_coursecode(coursecode):
+    timeblocks = read_from_json_file("static/timeblocks.json")
+    timeblocks_list = []
+    for timeblock in timeblocks:
+        if timeblock["kurskod"] == coursecode:
+            timeblocks_list.append(timeblock)
+    
+    return timeblocks_list
+
+
+
+
+
+
+
+
+
+
+
 @route("/")
 def index():
-    # läs från fil
-    
-    return template("mycourses")
+    # Lista av lexikon (kurs)
+    courses = read_from_json_file("static/courses.json")    
+    return template("mycourses", courses=courses)
 
 
 @route("/<coursecode>")
 def course_page(coursecode):
-    return template("coursepage", coursecode=coursecode)
+    # kontrollera att kurs med den kurskoden finns i courses.json
+    try:
+        course = get_course_with_coursecode(coursecode)
+        return template("coursepage", course=course)
+    except:
+        return template("error")
 
 
 @route("/new-course")
@@ -21,8 +93,30 @@ def new_course():
 
 @route("/add-course", method="post")
 def add_course():
-    courseCode = getattr(request.forms, "coursedropdown")
-    # skriv till fil...
+    courses = read_from_json_file("static/courses.json")
+    coursecode = getattr(request.forms, "coursedropdown")
+
+    # kontrollera att kursen inte redan är tillagd
+    for course in courses:
+        if course["kurskod"] == coursecode.upper():
+            print("Kursen finns redan")
+            return template("addcourse")
+    
+    # skriv till courses.json
+    course = {}
+
+    if coursecode == "da336a":
+        course["kurskod"] = "DA336A"
+        course["titel"] = "Systemutveckling och projekt"
+    elif coursecode == "da297a":
+        course["kurskod"] = "DA297A"
+        course["titel"] = "Databasteknik"
+    elif coursecode == "da108a":
+        course["kurskod"] = "DA108A"
+        course["titel"] = "Informationsarkitektur"
+
+    courses.append(course)
+    save_to_json_file(courses, "static/courses.json")
 
     # flash message("Kursen har lagts till")
     redirect("/")
@@ -30,7 +124,19 @@ def add_course():
 
 @route("/<coursecode>/tasks")
 def course_tasks(coursecode):
-    return template("tasks", coursecode=coursecode)
+    # läs in alla uppgifter för kurskod och skicka till tasks.html
+    try:
+        course = get_course_with_coursecode(coursecode)
+        tasks = get_tasks_with_coursecode(coursecode)
+        return template("tasks", course=course, tasks=tasks )
+    
+    except:
+        return template("error")
+    
+
+@route("/<coursecode>/tasks/<id>")
+def view_task():
+    pass
 
 
 @route("/add-task", method="post")
@@ -43,7 +149,19 @@ def add_task():
 
 @route("/<coursecode>/schedule")
 def course_tasks(coursecode):
-    return template("schedule", coursecode=coursecode)
+    # läs in alla tidsblock för kurskod och skicka till schedule.html
+    try:
+        course = get_course_with_coursecode(coursecode)
+        timeblocks = get_timeblocks_with_coursecode(coursecode)
+        return template("schedule", course=course, timeblocks=timeblocks)
+    
+    except:
+        return template("error")
+    
+
+@route("/<coursecode>/schedule/<id>")
+def view_timeblock():
+    pass
 
 
 @route("/add-timeblock", method="post")
