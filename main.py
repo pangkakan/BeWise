e02a2a5265fdd7cd06afc2f5c1a5809885d40799
Marcus Dentz <marcus.dentz93@gmail.com>
2024-04-14 13:27:51 +0200

    Funktionalitet gällande lägga in/visa kurser, uppgifter och tidsblock

--- main.py
+++ main.py
@@ -50,6 +50,13 @@ def get_tasks_with_coursecode(coursecode):
     return task_list
 
 
+def get_task_with_id(coursecode, id):
+    tasks = get_tasks_with_coursecode(coursecode)
+    for task in tasks:
+        if task["id"] == int(id):
+            return task
+
+
 def get_timeblocks_with_coursecode(coursecode):
     timeblocks = read_from_json_file("static/timeblocks.json")
     timeblocks_list = []
@@ -60,7 +67,20 @@ def get_timeblocks_with_coursecode(coursecode):
     return timeblocks_list
 
 
+def get_timeblock_with_id(coursecode, id):
+    timeblocks = get_timeblocks_with_coursecode(coursecode)
+    for timeblock in timeblocks:
+        if timeblock["id"] == int(id):
+            return timeblock
 
+
+# Generell funktion som tar emot listor av lexikon där man utgår från att alla lexikon har id-nyckel
+def create_id(list_of_dictionaries):
+    highest_id = 1
+    for dictionary in list_of_dictionaries:
+        if dictionary["id"] >= highest_id:
+            highest_id = dictionary["id"] + 1
+    return highest_id
 
 
 
@@ -71,7 +91,7 @@ def get_timeblocks_with_coursecode(coursecode):
 
 @route("/")
 def index():
-    # Lista av lexikon (kurs)
+    # Lista av kurser
     courses = read_from_json_file("static/courses.json")    
     return template("mycourses", courses=courses)
 
@@ -135,16 +155,44 @@ def course_tasks(coursecode):
     
 
 @route("/<coursecode>/tasks/<id>")
-def view_task():
-    pass
+def view_task(coursecode, id):
+    # hämta uppgift med rätt id från uppgiftslistan och skicka till task.html
+    try:
+        course = get_course_with_coursecode(coursecode)
+        task = get_task_with_id(coursecode, id)
+        return template("task", course=course, task=task)
+    
+    except:
+        return template("error")
 
 
 @route("/add-task", method="post")
 def add_task():
-    # skriv till fil
+    coursecode = getattr(request.forms, "coursecode")
+    task_title = getattr(request.forms, "task_title")
+    task_date = getattr(request.forms, "task_date")
+
+    # hämta alla inlagda uppgifter
+    all_tasks = read_from_json_file("static/tasks.json")
+    # skapa unikt id för uppgift
+    task_id = create_id(all_tasks)
+
+    new_task = {
+        "id": task_id,
+        "kurskod": coursecode,
+        "titel": task_title,
+        "datum": task_date
+    }
+    # lägg till den nya uppgiften i uppgiftslistan
+    all_tasks.append(new_task)
+
+    # skriv till tasks.json
+    save_to_json_file(all_tasks, "static/tasks.json")
+
 
     # flash message("Uppgiften har lagts till")
-    pass
+    # istället för redirect till startsidan kan detta lösas med htmx så man stannar kvar på sidan
+    redirect("/")
 
 
 @route("/<coursecode>/schedule")
@@ -160,16 +208,48 @@ def course_tasks(coursecode):
     
 
 @route("/<coursecode>/schedule/<id>")
-def view_timeblock():
-    pass
+def view_timeblock(coursecode, id):
+    # hämta tidsblock med rätt id från tidsblocklistan och skicka till timeblock.html
+    try:
+        course = get_course_with_coursecode(coursecode)
+        timeblock = get_timeblock_with_id(coursecode, id)
+        return template("timeblock", course=course, timeblock=timeblock)
+            
+    except:
+        return template("error")
 
 
 @route("/add-timeblock", method="post")
 def add_timeblock():
-    # skriv till fil
+    coursecode = getattr(request.forms, "coursecode")
+    timeblock_title = getattr(request.forms, "timeblock_title")
+    timeblock_date = getattr(request.forms, "timeblock_date")
+    timeblock_start_time = getattr(request.forms, "timeblock_start_time")
+    timeblock_end_time = getattr(request.forms, "timeblock_end_time")
+
+    # hämta alla inlagda tidsblock
+    all_timeblocks = read_from_json_file("static/timeblocks.json")
+    # skapa unikt id för uppgift
+    timeblock_id = create_id(all_timeblocks)
+
+    new_timeblock = {
+        "id": timeblock_id,
+        "kurskod": coursecode,
+        "titel": timeblock_title,
+        "datum": timeblock_date,
+        "starttid": timeblock_start_time,
+        "sluttid": timeblock_end_time
+    }
+    # lägg till den nya uppgiften i uppgiftslistan
+    all_timeblocks.append(new_timeblock)
+
+    # skriv till timeblocks.json
+    save_to_json_file(all_timeblocks, "static/timeblocks.json")
+
 
     # flash message("Tidsblocket har lagts till")
-    pass
+    # istället för redirect till startsidan kan detta lösas med htmx så man stannar kvar på sidan
+    redirect("/")
 
 
 @route("/preferences")
