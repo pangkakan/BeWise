0ab7da2d340cc29c04efb64a294104bb55a38e0d
pangkakan <115007208+pangkakan@users.noreply.github.com>
2024-05-09 11:04:40 +0200

    nil

--- main/models/events.py
+++ main/models/events.py
@@ -5,7 +5,7 @@ def get_events(conn):
     cur = conn.cursor()
     # Fetch events and convert date and time columns to string format
     query = """
-    SELECT id, date::text, start_time::text, end_time::text, location, description FROM events
+    SELECT id, date::text, start_time::text, end_time::text, location, description FROM course_events
     """
     cur.execute(query)
     events = cur.fetchall()
@@ -21,7 +21,8 @@ def scrape_to_db(conn, course_code, course_or_no):
     for week_events in events.values():
         for event in week_events:
             cur.execute(
-                "INSERT INTO course_events (day, date, start_time, end_time, location, description, course_code) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                "INSERT INTO course_events (day, date, start_time, end_time, location, description, course_code)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s)",
                 (
                     event["day"],
                     event["date"],
