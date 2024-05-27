c726cb35009ec485b800ef71cf8f921bcd198d04
Marcus Dentz <marcus.dentz93@gmail.com>
2024-05-27 13:24:55 +0200

    Visa kurser, mål, uppgifter och deluppgifter

--- main/main.py
+++ main/main.py
@@ -292,28 +292,122 @@ def get_user_goals():
 def view_courses():
     courses = ["DA336A - Systemutveckling startdaum slutdatum", "DA297A - Databasteknik startdatum slutdatum", "DA354A - Programmering startdatum slutdatum"]
     # get all user courses (id, coursecode, title, start, end) from database
-    return template("view-courses-list", courses=courses)
+
+    cur = conn.cursor()
+
+    query = """
+    SELECT c.title, uc.id, c.start_timestamp, c.end_timestamp, c.course_code FROM users u 
+    JOIN user_courses uc ON u.id = uc.user_id
+    JOIN courses c ON uc.course_id = c.id
+    WHERE user_id = %s
+     
+    """ % current_user
+    cur.execute(query)
+    this_user_courses = cur.fetchall()
+    course_list = []
+    for course in this_user_courses:
+        course = {
+            "title": course[0],
+            "id": course[1],
+            "start": course[2],
+            "end": course[3],
+            "code": course[4]
+        }
+        course_list.append(course)
+
+    return template("view-courses-list", courses=course_list)
 
 
 @route("/view-goals")
 def view_goals():
     goals = ["Mål 1", "Mål 2", "Mål 3"]
     # get all user goals (id, course, title, start, end, type, completed) from database
-    return template("view-goals-list", goals=goals)
+    cur = conn.cursor()
+    query = """
+    SELECT g.id, g.title, g.start_time, g.deadline_timestamp, g.type, g.completed FROM users u 
+    JOIN user_courses uc ON u.id = uc.user_id
+    JOIN courses c ON uc.course_id = c.id
+    JOIN public.goals g on uc.id = g.user_course_id
+    WHERE user_id = %s
+     
+    """ % current_user
+    cur.execute(query)
+    this_user_goals = cur.fetchall()
+    goal_list = []
+    for goal in this_user_goals:
+        goal = {
+            "id": goal[0],
+            "title": goal[1],
+            "start": goal[2],
+            "end": goal[3],
+            "type": goal[4],
+            "completed": goal[5]
+        }
+        goal_list.append(goal)
+
+    return template("view-goals-list", goals=goal_list)
 
 
 @route("/view-assignments")
 def view_assignments():
     assignments = ["Uppgift 1", "Uppgift 2", "Uppgift 3"]
     # get all user assignments (id, goal, title, start, end, type, priority, completed) from database
-    return template("view-assignments-list", assignments=assignments)
+    cur = conn.cursor()
+    query = """
+    SELECT a.id, a.title, a.start_time, a.deadline_timestamp, a.type, a.priority, a.completed FROM users u 
+    JOIN user_courses uc ON u.id = uc.user_id
+    JOIN courses c ON uc.course_id = c.id
+    JOIN goals g on uc.id = g.user_course_id
+    JOIN assignments a on g.id = a.goal_id
+    WHERE user_id = %s
+     
+    """ % current_user
+    cur.execute(query)
+    this_user_assignments = cur.fetchall()
+    assignment_list = []
+    for assignment in this_user_assignments:
+        assignment = {
+            "id": assignment[0],
+            "title": assignment[1],
+            "start": assignment[2],
+            "end": assignment[3],
+            "type": assignment[4],
+            "priority": assignment[5],
+            "completed": assignment[6]
+        }
+        assignment_list.append(assignment)
+
+    return template("view-assignments-list", assignments=assignment_list)
 
 
 @route("/view-subtasks")
 def view_subtasks():
     subtasks = ["Deluppgift 1", "Deluppgift 2", "Deluppgift 3"]
     # get all user subtasks (id, assignment, title, date, type, completed) from database
-    return template("view-subtasks-list", subtasks=subtasks)
+
+    cur = conn.cursor()
+    query = """
+    SELECT s.title, s.id, s.date, s.completed FROM users u 
+    JOIN user_courses uc ON u.id = uc.user_id
+    JOIN courses c ON uc.course_id = c.id
+    JOIN goals g on uc.id = g.user_course_id
+    JOIN assignments a on g.id = a.goal_id
+    JOIN subtasks s on a.id = s.assignment_id
+    WHERE user_id = %s
+     
+    """ % current_user
+    cur.execute(query)
+    this_user_subtasks = cur.fetchall()
+    subtask_list = []
+    for subtask in this_user_subtasks:
+        subtask = {
+            "title": subtask[0],
+            "id": subtask[1],
+            "date": subtask[2],
+            "completed": subtask[3]
+        }
+        subtask_list.append(subtask)
+    return template("view-subtasks-list", subtasks=subtask_list)
 
 
 @route("/view-events")
